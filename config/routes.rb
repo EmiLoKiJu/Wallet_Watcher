e1d0be1831b317b11d4d4041a0a1bd296663c050
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-09-15 00:17:31 -0800

    Add css styles, classes and positions for most pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_scope :user do
+    get "/custom_sign_out" => "devise/sessions#destroy", as: :custom_destroy_user_session
+  end
+
   root 'splash_screen#index'
   get '/splash_screen', to: 'splash_screen#index', as: 'splash_screen'
   resources :groups, only: [:index, :show, :new, :create] do
